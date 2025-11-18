202f7d089ef422135f6fd2dbf31bd7e664ced0ed
wpzy.cc <138046438+wpzycc@users.noreply.github.com>
2025-11-18 11:34:29 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-transparent py-4 text-center text-sm text-gray-600 dark:text-gray-400">
-    <div>© {{ new Date().getFullYear() }} made with ♥️ by <a class="text-purple-600" href="https://wpzy.cc">AQ网盘论坛</a></div>
+    <div>© {{ new Date().getFullYear() }} made with ♥️ by <a class="text-purple-600" href="https://wpzy.cc" target="_blank">AQ网盘论坛</a></div>
     <div>已稳定运行: {{ uptime }}</div> 
   </footer>
 </template>
