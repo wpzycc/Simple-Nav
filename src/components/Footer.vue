908b53bf48ca209f38b1c7990c3cba8467df3574
wpzy.cc <138046438+wpzycc@users.noreply.github.com>
2025-11-18 10:57:52 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
 export default {
   data() {
     return {
-      startDate: new Date('2025-03-01'),
+      startDate: new Date('2025-11-18'),
       currentYear: new Date().getFullYear(),
       now: new Date() // 新增当前时间响应式数据
     };
